d3c5ff8d610190448727e1349d6a990feb4ed24e
katychuang <katychuang@gmail.com>
2015-09-16 19:27:21 -0400

    some of hadoop config strings that need to be added

--- scripts/hadoop_install.sh
+++ scripts/hadoop_install.sh
@@ -26,4 +26,53 @@ hdfs -version
 
 # hadoop configs
 # hadoop-namenode-config.sh
+
+: '
+sudo vim $HADOOP_HOME/etc/hadoop/hadoop-env.sh
+Find the line with export JAVA_HOME=${JAVA_HOME} and replace the ${JAVA_HOME} with /usr
+
+export JAVA_HOME=/usr
+'
+
+# core-site
+: '
+edit $HADOOP_HOME/etc/hadoop/core-site.xml
+all point to namenode
+<property>
+   <name>fs.defaultFS</name>
+   <value>hdfs://masternode_public_dns:9000</value>
+</property>
+'
+
+: '
+<property>
+   <name>yarn.nodemanager.aux-services</name>
+   <value>mapreduce_shuffle</value>
+</property>
+   
+<property>
+   <name>yarn.nodemanager.aux-services.mapreduce.shuffle.class</name>
+   <value>org.apache.hadoop.mapred.ShuffleHandler</value>
+</property>
+
+<property>
+   <name>yarn.resourcemanager.resource-tracker.address</name>
+   <value>namenode-public-dns:8025</value>
+</property>
+
+<property>
+   <name>yarn.resourcemanager.scheduler.address</name>
+   <value>namenode-public-dns:8030</value>
+</property>
+
+<property>
+   <name>yarn.resourcemanager.address</name>
+   <value>namenode-public-dns:8050</value>
+</property>
+'
+
 # hadoop-datanode-config.sh
+
+
+
+
