cfd22ec7edceb5c4794600e09d00e092ba848564
katychuang <katychuang@gmail.com>
2015-09-19 23:21:53 -0400

    include gradle commands to samza install

--- scripts/samza_install.sh
+++ scripts/samza_install.sh
@@ -10,4 +10,12 @@ source ~/.zshrc
 
 echo $SAMZA_HOME
 
+sudo chown -R ubuntu $SAMZA_HOME
 
+# gradle for building project
+wget https://services.gradle.org/distributions/gradle-2.7-all.zip -P ~/Downloads
+sudo unzip -a ~/Downloads/gradle-2.7-all.zip -d /usr/local
+sudo mv /usr/local/gradle-2.7 /usr/local/gradle
+
+gradle -b bootstrap.gradle
+./gradlew clean build
