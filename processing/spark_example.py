a588c5bf9073bd21964671e11b5ea7781da8c517
katychuang <katychuang@gmail.com>
2015-09-22 11:10:53 -0400

    printing spark stream

--- processing/spark_example.py
+++ processing/spark_example.py
@@ -15,3 +15,11 @@ words = lines.flatMap(lambda line: line.split(" "))
 # Count each word in each batch
 pairs = words.map(lambda word: (word, 1))
 wordCounts = pairs.reduceByKey(lambda x, y: x + y)
+
+pos = lines.map(lambda line: line.split(',')).filter(lambda line: line[0] > 1)
+
+# Print the first ten elements of each RDD generated in this DStream to the console
+wordCounts.pprint()
+
+ssc.start()             # Start the computation
+ssc.awaitTermination()  # Wait for the computation to terminate
